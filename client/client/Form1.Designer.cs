2342115f8be6d9e41535ca2e4ce6d841217e287c
cemrtkn <73444238+cemrtkn@users.noreply.github.com>
2021-12-12 02:40:41 +0300

    Add files via upload

--- client/client/Form1.Designer.cs
+++ client/client/Form1.Designer.cs
@@ -44,50 +44,49 @@
             this.button_users = new System.Windows.Forms.Button();
             this.textBox_follow = new System.Windows.Forms.TextBox();
             this.button_follow = new System.Windows.Forms.Button();
+            this.checkBox_onlyfollows = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(67, 80);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(89, 100);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(28, 20);
+            this.label1.Size = new System.Drawing.Size(37, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "IP:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 122);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(69, 152);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.Size = new System.Drawing.Size(57, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "Port:";
             // 
             // textBox_ip
             // 
-            this.textBox_ip.Location = new System.Drawing.Point(100, 78);
-            this.textBox_ip.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_ip.Location = new System.Drawing.Point(133, 98);
+            this.textBox_ip.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox_ip.Name = "textBox_ip";
-            this.textBox_ip.Size = new System.Drawing.Size(130, 26);
+            this.textBox_ip.Size = new System.Drawing.Size(172, 31);
             this.textBox_ip.TabIndex = 2;
             // 
             // textBox_port
             // 
-            this.textBox_port.Location = new System.Drawing.Point(100, 122);
-            this.textBox_port.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_port.Location = new System.Drawing.Point(133, 152);
+            this.textBox_port.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox_port.Name = "textBox_port";
-            this.textBox_port.Size = new System.Drawing.Size(130, 26);
+            this.textBox_port.Size = new System.Drawing.Size(172, 31);
             this.textBox_port.TabIndex = 3;
             // 
             // button_connect
             // 
-            this.button_connect.Location = new System.Drawing.Point(100, 224);
-            this.button_connect.Margin = new System.Windows.Forms.Padding(2);
+            this.button_connect.Location = new System.Drawing.Point(133, 280);
+            this.button_connect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button_connect.Name = "button_connect";
-            this.button_connect.Size = new System.Drawing.Size(104, 34);
+            this.button_connect.Size = new System.Drawing.Size(139, 42);
             this.button_connect.TabIndex = 4;
             this.button_connect.Text = "connect";
             this.button_connect.UseVisualStyleBackColor = true;
@@ -95,40 +94,39 @@
             // 
             // logs
             // 
-            this.logs.Location = new System.Drawing.Point(430, 68);
-            this.logs.Margin = new System.Windows.Forms.Padding(2);
+            this.logs.Location = new System.Drawing.Point(573, 85);
+            this.logs.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.logs.Name = "logs";
             this.logs.ReadOnly = true;
-            this.logs.Size = new System.Drawing.Size(301, 386);
+            this.logs.Size = new System.Drawing.Size(400, 482);
             this.logs.TabIndex = 5;
             this.logs.Text = "";
             // 
             // textBox_message
             // 
             this.textBox_message.Enabled = false;
-            this.textBox_message.Location = new System.Drawing.Point(100, 422);
-            this.textBox_message.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_message.Location = new System.Drawing.Point(133, 528);
+            this.textBox_message.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox_message.Name = "textBox_message";
-            this.textBox_message.Size = new System.Drawing.Size(145, 26);
+            this.textBox_message.Size = new System.Drawing.Size(192, 31);
             this.textBox_message.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 425);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(20, 531);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.Size = new System.Drawing.Size(106, 25);
             this.label3.TabIndex = 7;
             this.label3.Text = "Message:";
             // 
             // button_send
             // 
             this.button_send.Enabled = false;
-            this.button_send.Location = new System.Drawing.Point(249, 414);
-            this.button_send.Margin = new System.Windows.Forms.Padding(2);
+            this.button_send.Location = new System.Drawing.Point(332, 518);
+            this.button_send.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button_send.Name = "button_send";
-            this.button_send.Size = new System.Drawing.Size(98, 40);
+            this.button_send.Size = new System.Drawing.Size(131, 50);
             this.button_send.TabIndex = 8;
             this.button_send.Text = "send";
             this.button_send.UseVisualStyleBackColor = true;
@@ -136,28 +134,29 @@
             // 
             // textBox_username
             // 
-            this.textBox_username.Location = new System.Drawing.Point(100, 170);
-            this.textBox_username.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_username.Location = new System.Drawing.Point(133, 212);
+            this.textBox_username.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox_username.Name = "textBox_username";
-            this.textBox_username.Size = new System.Drawing.Size(130, 26);
+            this.textBox_username.Size = new System.Drawing.Size(172, 31);
             this.textBox_username.TabIndex = 10;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(9, 173);
+            this.label4.Location = new System.Drawing.Point(12, 216);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(84, 20);
+            this.label4.Size = new System.Drawing.Size(113, 25);
             this.label4.TabIndex = 11;
             this.label4.Text = "username:";
             // 
             // button_feed
             // 
             this.button_feed.Enabled = false;
-            this.button_feed.Location = new System.Drawing.Point(430, 25);
-            this.button_feed.Margin = new System.Windows.Forms.Padding(2);
+            this.button_feed.Location = new System.Drawing.Point(573, 31);
+            this.button_feed.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button_feed.Name = "button_feed";
-            this.button_feed.Size = new System.Drawing.Size(301, 39);
+            this.button_feed.Size = new System.Drawing.Size(401, 49);
             this.button_feed.TabIndex = 12;
             this.button_feed.Text = "Request Feed";
             this.button_feed.UseVisualStyleBackColor = true;
@@ -166,10 +165,10 @@
             // button_disconnect
             // 
             this.button_disconnect.Enabled = false;
-            this.button_disconnect.Location = new System.Drawing.Point(224, 224);
-            this.button_disconnect.Margin = new System.Windows.Forms.Padding(2);
+            this.button_disconnect.Location = new System.Drawing.Point(299, 280);
+            this.button_disconnect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button_disconnect.Name = "button_disconnect";
-            this.button_disconnect.Size = new System.Drawing.Size(104, 34);
+            this.button_disconnect.Size = new System.Drawing.Size(139, 42);
             this.button_disconnect.TabIndex = 13;
             this.button_disconnect.Text = "disconnect";
             this.button_disconnect.UseVisualStyleBackColor = true;
@@ -178,10 +177,10 @@
             // button_users
             // 
             this.button_users.Enabled = false;
-            this.button_users.Location = new System.Drawing.Point(100, 279);
-            this.button_users.Margin = new System.Windows.Forms.Padding(2);
+            this.button_users.Location = new System.Drawing.Point(133, 349);
+            this.button_users.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button_users.Name = "button_users";
-            this.button_users.Size = new System.Drawing.Size(228, 32);
+            this.button_users.Size = new System.Drawing.Size(304, 40);
             this.button_users.TabIndex = 14;
             this.button_users.Text = "Users";
             this.button_users.UseVisualStyleBackColor = true;
@@ -190,29 +189,41 @@
             // textBox_follow
             // 
             this.textBox_follow.Enabled = false;
-            this.textBox_follow.Location = new System.Drawing.Point(430, 475);
-            this.textBox_follow.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_follow.Location = new System.Drawing.Point(573, 594);
+            this.textBox_follow.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox_follow.Name = "textBox_follow";
-            this.textBox_follow.Size = new System.Drawing.Size(145, 26);
+            this.textBox_follow.Size = new System.Drawing.Size(192, 31);
             this.textBox_follow.TabIndex = 15;
             // 
             // button_follow
             // 
             this.button_follow.Enabled = false;
-            this.button_follow.Location = new System.Drawing.Point(595, 467);
-            this.button_follow.Margin = new System.Windows.Forms.Padding(2);
+            this.button_follow.Location = new System.Drawing.Point(793, 584);
+            this.button_follow.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button_follow.Name = "button_follow";
-            this.button_follow.Size = new System.Drawing.Size(136, 43);
+            this.button_follow.Size = new System.Drawing.Size(181, 54);
             this.button_follow.TabIndex = 16;
             this.button_follow.Text = "follow";
             this.button_follow.UseVisualStyleBackColor = true;
             this.button_follow.Click += new System.EventHandler(this.button_follow_Click);
             // 
+            // checkBox_onlyfollows
+            // 
+            this.checkBox_onlyfollows.AutoSize = true;
+            this.checkBox_onlyfollows.Enabled = false;
+            this.checkBox_onlyfollows.Location = new System.Drawing.Point(399, 42);
+            this.checkBox_onlyfollows.Name = "checkBox_onlyfollows";
+            this.checkBox_onlyfollows.Size = new System.Drawing.Size(167, 29);
+            this.checkBox_onlyfollows.TabIndex = 17;
+            this.checkBox_onlyfollows.Text = "Only Follows";
+            this.checkBox_onlyfollows.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(762, 554);
+            this.ClientSize = new System.Drawing.Size(1016, 692);
+            this.Controls.Add(this.checkBox_onlyfollows);
             this.Controls.Add(this.button_follow);
             this.Controls.Add(this.textBox_follow);
             this.Controls.Add(this.button_users);
@@ -229,7 +240,7 @@
             this.Controls.Add(this.textBox_ip);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -255,6 +266,7 @@
         private System.Windows.Forms.Button button_users;
         private System.Windows.Forms.TextBox textBox_follow;
         private System.Windows.Forms.Button button_follow;
+        private System.Windows.Forms.CheckBox checkBox_onlyfollows;
     }
 }
 
